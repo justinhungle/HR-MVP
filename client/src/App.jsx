67315fe25aed60d875ec3dc3a21cd9aa294f4c87
Justin Le <justinhung.le@gmail.com>
2022-01-23 18:47:53 -0800

    Set up skeleton for my app

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+
+  return (
+    <div>My App</div>
+  )
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, "client/src", "index.js"),
+  output: {
+    filename: 'bundle.js',
+    path:path.resolve(__dirname, "public/dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ]
+  },
+  plugins: [
+    new CompressionPlugin,
+  ],
+}
